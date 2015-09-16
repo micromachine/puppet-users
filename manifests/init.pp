f96e7052dfb2eb484727d949f6db05c30343b82a
root <root@foreman.test.pl>
2015-09-16 11:42:38 +0200

    group per user

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,37 @@
 class accounts (
   $users2add  = ['greg','greg2'],
-  $group2add  = ['superadmins']
+  $group2add  = 'superadmins'
 )  {
 
-define ceateaccount {
+define ceateaccount($group) {
 
   file { "/home/$name":
     ensure => directory,
     mode   => 0750,
   }
 
+  group { $name:
+    ensure => "present",
+  } 
+
+
   user { $name:
     ensure => present,
-    gid    =>  10000,
     home   =>  "/home/${name}",
     managehome =>  true,
+    groups => [$name,$group],
+    require    => Group[$group],
+
   }
-
-
 }
 group { $group2add:
     ensure => "present",
     gid    => 10000, 
-  }
+  } 
 
-ceateaccount {$users2add:}
+ceateaccount {$users2add:
+	group => $group2add,
+		}
 
 }
 
